2ab98cc0e6ae2cc42b376b9d2f26409146c0b084
sandeepveera99 <137384076+sandeepveera99@users.noreply.github.com>
2023-06-22 16:45:59 +0530

    Add files via upload

---
+++ spinproject/spinningwheel.js
@@ -0,0 +1,8 @@
+function closeloadingpage(){
+    document.getElementById("loading").style.display="none"
+}
+function rotatewheel(){
+    var randomNum=Math.floor(Math.random()*360);
+    document.getElementById("wheel").style.transition="rotate 10s"
+    document.getElementById("wheel").style.rotate=randomNum*10+"deg"
+}
